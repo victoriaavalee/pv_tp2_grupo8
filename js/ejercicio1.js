db0a8fb18efe4b51c1f98ed30807352a999aee3e
paola-zoe <paolazoetoconas@gmail.com>
2025-04-17 13:00:17 -0300

    Ejercicios 1, 9 y 10

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,8 @@
+/*1.Crear una variable denominada integrantes y asignarle la cadena de texto con los 
+nombres de los integrantes del grupo.
+Mostrar por consola el siguiente texto:
+Somos (el valor de la variable) y estamos aprendiendo javascript.*/
 
+const integrantes = "Facundo, Lara, Paola, Patricia y Victoria";
+
+console.log("Somos " + integrantes + " y estamos aprendiendo javascript.")
