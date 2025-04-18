27ff77ecfa9c49db43b01f99ac55b9375c70f887
Carhuavilca Velasquez <vero03velasquez@gmail.com>
2025-04-17 21:02:35 -0300

    js ejercicio23

---
+++ js/ejercicio23.js
@@ -0,0 +1,25 @@
+
+
+function mostrarImagen1()
+{ 
+    document.getElementById("img1").style.display = "block";
+    document.getElementById("img2").style.display = "none";
+    document.getElementById("img3").style.display = "none";
+ }
+
+
+ function mostrarImagen2()
+{ 
+    document.getElementById("img1").style.display = "none";
+    document.getElementById("img2").style.display = "block";
+    document.getElementById("img3").style.display = "none";
+}
+
+function mostrarImagen3()
+{ 
+    document.getElementById("img1").style.display = "none";
+    document.getElementById("img2").style.display = "none";
+    document.getElementById("img3").style.display = "block";
+}
+
+
